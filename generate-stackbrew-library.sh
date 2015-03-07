fd21f40b5e96f787db620ae4eea6c0c5e476f510
Moghedrin <nolat301@gmail.com>
2015-03-06 17:17:51 -0700

    Add update and stackbrew scripts

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+url='git://github.com/docker-library/julia'
+commit=$(git log -1 --format="format:%H" -- Dockerfile)
+fullVersion="$(grep -m1 'ENV JULIA_VERSION' Dockerfile | cut -d' ' -f3)"
+version="${fullVersion%[.-]*}"
+
+echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
+echo
+echo "$fullVersion: ${url}@${commit}"
+echo "$version: ${url}@${commit}"
+echo "latest: ${url}@${commit}"
