34430d8ce37dc9f499007079fdf550ea331ff4ff
Moghedrin <nolat301@gmail.com>
2015-03-06 17:20:29 -0700

    Clean up a line

--- update.sh
+++ update.sh
@@ -3,8 +3,7 @@ set -e
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 pattern='.*\/julia-([0-9]+\.[0-9]+\.[0-9]+)-linux-x86_64\.tar\.gz.*'
-version=$(curl -sSL 'http://julialang.org/downloads/' \
-	| sed -rn "s/${pattern}/\1/gp")
+version=$(curl -sSL 'http://julialang.org/downloads/' | sed -rn "s/${pattern}/\1/gp")
 
 sed -ri 's/^(ENV JULIA_VERSION) .*/\1 '"$version"'/' "Dockerfile"
 
