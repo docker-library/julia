7cdd4477e389248fd42f9bf6a5d34324be1fbdc4
Tianon Gravi <admwiggin@gmail.com>
2016-06-09 11:26:45 -0700

    Convert "generate-stackbrew-library.sh" to output the new 2822-based format

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,14 +1,59 @@
 #!/bin/bash
-set -e
+set -eu
 
+self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
-url='git://github.com/docker-library/julia'
-commit="$(git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
-fullVersion="$(grep -m1 'ENV JULIA_VERSION' Dockerfile | cut -d' ' -f3)"
-version="${fullVersion%[.-]*}"
 
-echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
+# get the most recent commit which modified any of "$@"
+fileCommit() {
+	git log -1 --format='format:%H' HEAD -- "$@"
+}
+
+# get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
+dirCommit() {
+	local dir="$1"; shift
+	(
+		cd "$dir"
+		fileCommit \
+			Dockerfile \
+			$(git show HEAD:./Dockerfile | awk '
+				toupper($1) == "COPY" {
+					for (i = 2; i < NF; i++) {
+						print $i
+					}
+				}
+			')
+	)
+}
+
+cat <<-EOH
+# this file is generated via https://github.com/docker-library/julia/blob/$(fileCommit "$self")/$self
+
+Maintainers: Tianon Gravi <admwiggin@gmail.com> (@tianon),
+             Joseph Ferguson <yosifkit@gmail.com> (@yosifkit)
+GitRepo: https://github.com/docker-library/julia.git
+EOH
+
+# prints "$2$1$3$1...$N"
+join() {
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
+
+commit="$(dirCommit .)"
+
+fullVersion="$(git show "$commit":Dockerfile | awk '$1 == "ENV" && $2 == "JULIA_VERSION" { print $3; exit }')"
+
+versionAliases=()
+while [ "${fullVersion%.*}" != "$fullVersion" ]; do
+	versionAliases+=( $fullVersion )
+	fullVersion="${fullVersion%.*}"
+done
+versionAliases+=( $fullVersion latest )
+
 echo
-echo "$fullVersion: ${url}@${commit}"
-echo "$version: ${url}@${commit}"
-echo "latest: ${url}@${commit}"
+cat <<-EOE
+	Tags: $(join ', ' "${versionAliases[@]}")
+	GitCommit: $commit
+EOE
